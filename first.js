75f56585ca3933bd8d888f31b0b81e70d6acf223
Umarfarooq88 <umarat372@gmail.com>
2024-07-24 16:03:15 +0530

    Object printing types

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log("Just trying");
+
+//------> object
+const company={
+    name: "Fin",
+    employee: "Umar farooq",
+    Id: 733713
+}
+
+console.log(company); /// to print whole object
+console.log(company["employee"]);// to print value of particular key in object
+console.log(company.employee);
